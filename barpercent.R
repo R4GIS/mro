866e081659d5b476107f667d55814808d80eb388
jtr13 <jtr13@users.noreply.github.com>
2016-04-19 16:26:37 -0400

    Allow user to pass color to function

--- barpercent.R
+++ barpercent.R
@@ -1,5 +1,5 @@
 # draws a horizontal bar chart with percentages labeled
-barpercent <- function (x, names = NULL) {
+barpercent <- function (x, names = NULL, col = "lightblue") {
     if (is.null(names)) names <- names(x)
     par (las = 1, mar = c(5,10,4,6), xpd = NA, xaxs = "i", yaxs= "i")
     data <- x[order(x)]*100/sum(x)
@@ -10,7 +10,7 @@ barpercent <- function (x, names = NULL) {
     plot(NULL, xlim = c(0,xmax), ylim=c(0,ymax), axes = FALSE,
          xlab = "percent", ylab="")
     barplot(data, horiz = TRUE, names.arg = labels,
-            col = "lightblue", border = "lightblue",
+            col = col, border = col,
             axes = FALSE, add = TRUE)
     axis (1, pos = 0, xlim = c(0,xmax), at = c(0,1:(xmax/10)*10))
     hat <- -xmax/16.66667
@@ -25,3 +25,4 @@ barpercent <- function (x, names = NULL) {
 # x <- c(4, 23, 45)
 # names(x) <- c("Region 1","Region 2","Region 3")
 # barpercent (x)
+# barpercent (x, col = "rosybrown1")
